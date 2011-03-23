f98f80c157b42ba94c3a8e1b936ad0c1c642eaaa
Dmitriy Samoilov <e2718281828@ya.ru>
2011-03-23 23:25:37 +0200

    split RequestQueue class into SimpleRequestQueue (to store only number of requests) and RequestQueue (store distinct urls array) classes

---
+++ lib/requests_queue.rb
@@ -0,0 +1,30 @@
+class RequestsQueue
+  def initialize(url_array)
+    @urls_queue = url_array
+    @locked_urls = []
+
+    @num_of_requests = url_array.size
+    @num_of_locked   = 0
+  end
+
+  def lock_next_request
+    if @num_of_requests > @num_of_locked
+      @num_of_locked += 1
+      @locked_urls << @urls_queue.shift
+      @locked_urls.last
+    end
+  end
+
+  def release_locked_request(url)
+    if @num_of_locked > 0
+      @num_of_locked -= 1
+      @locked_urls.delete_if{|u| u == url}
+    end
+
+    @num_of_requests -= 1 if @num_of_requests > 0
+  end
+
+  def empty?
+    @num_of_requests == 0
+  end
+end
---
+++ lib/simple_requests_queue.rb
@@ -0,0 +1,24 @@
+class SimpleRequestsQueue
+  def initialize(num_of_requests)
+    @num_of_requests = num_of_requests
+    @num_of_locked   = 0
+  end
+
+  def lock_next_request
+    if @num_of_requests > @num_of_locked
+      @num_of_locked += 1
+      true
+    else
+      false
+    end
+  end
+
+  def release_locked_request(url)
+    @num_of_locked -= 1 if @num_of_locked > 0    
+    @num_of_requests -= 1 if @num_of_requests > 0
+  end
+
+  def empty?
+    @num_of_requests == 0
+  end
+end
